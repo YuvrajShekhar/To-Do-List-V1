a24c9924304c88d458e4704cfca821737317076d
yuvraj <syuvraj1999@gamil.com>
2021-07-04 18:15:10 +0530

    added functionality and css

--- app.js
+++ app.js
@@ -2,20 +2,38 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
-app.get("/", function(req, res){
-  
-    var today = new Date();
-    var currentDay = today.getDay();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-    if ( currentDay == 6 || currentDay == 0){
-        res.send("<h1>Yay! its a weekend.</h1>")
-    }
-    else{
-        res.sendFile(__dirname + "/index.html");
-    }
+
+app.get("/", function (req, res) {
+
+    let today = new Date();
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-Us", options);
+
+    res.render('list', { kindOfDay: day, newListItems: items });
 });
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+
+app.post("/", function (req, res) {
+    item = req.body.newItem;
+    console.log(item);
+
+    items.push(item);
+ 
+    res.redirect("/");
+})
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000.");
 });
